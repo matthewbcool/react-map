db72c4fadba4cf302dbe46f29e282c264f5eeb2f
Matt Cool <matthewbcool@gmail.com>
2018-10-01 13:58:02 -0500

    added api request and set response to state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import axios from 'axios'
 class App extends Component {
-
+  constructor(){
+    super()
+    this.state = {
+      restaurants: [],
+    } 
+}
 componentDidMount() {
+  this.getVenues()
   this.createMap()
 }
   createMap = () => {
     scriptLoader("https://maps.googleapis.com/maps/api/js?key=AIzaSyDyJ4DkX9g-x6blTKDjDdf9EI2_mGKRTPM&callback=initMap")
     window.initMap = this.initializeMap
   }
+  
 
   initializeMap = () => {
   const map = new window.google.maps.Map(document.getElementById('map'), {
@@ -17,6 +24,25 @@ componentDidMount() {
     zoom: 15
   });
 }
+
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: '0H1LM44LJGDK2FF0VXA21UKOQDC2VSCNV0OULKFEFTFZLRGP',
+      client_secret: 'L4XBJ54FX5LPKNCA1ZNBORA2F00WZ5GHXK0OQ4BJIZRNP4AC',
+      query: 'restaurant',
+      near: 'Logan+Square',
+      v: "20182507"
+    }
+    axios.get(endPoint + new URLSearchParams(parameters)).then(
+      response => {
+        this.setState( {restaurants: response.data} )
+      }
+    ).catch(error => {
+      console.log("err" + error)
+    }) 
+  }
+
 
   render() {
     return (
@@ -26,7 +52,7 @@ componentDidMount() {
     );
   }
 }
-
+//0H1LM44LJGDK2FF0VXA21UKOQDC2VSCNV0OULKFEFTFZLRGP  L4XBJ54FX5LPKNCA1ZNBORA2F00WZ5GHXK0OQ4BJIZRNP4AC
 function scriptLoader(url) {
   const index = window.document.getElementsByTagName("script")[0]
   const script = window.document.createElement("script")
